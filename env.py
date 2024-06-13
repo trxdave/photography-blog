f3cf9d2bb87c1c0d4b73e3e2e224f77f4c00c9db
David Cotter <daveguy28@gmail.com>
2024-06-13 14:32:34 +0000

    env update the postgres

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ.setdefault("DATABASE_URL", 'postgres://uvecwwkfjxv:b5yV64lh2EqM@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/chop_wagon_alike_318732')
-os.environ.setdefault("SECRET_KEY", 'django-insecure-u$kptmbry7*t3p_7l+66zf@*ky@m-7$9fm=#ef$$yjpim_4$=e')
-os.environ.setdefault("CLOUDINARY_URL", 'cloudinary://328897878487583:hhq3G20ZpZGq_2TLzQ4SGFXXra8@ds0ualpzt')
+os.environ.setdefault("DATABASE_URL", "postgres://uvecwwkfjxv:b5yV64lh2EqM@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/chop_wagon_alike_318732")
+os.environ.setdefault("SECRET_KEY", "django-insecure-u$kptmbry7*t3p_7l+66zf@*ky@m-7$9fm=#ef$$yjpim_4$=e")
+os.environ.setdefault("CLOUDINARY_URL", "cloudinary://328897878487583:hhq3G20ZpZGq_2TLzQ4SGFXXra8@ds0ualpzt")
