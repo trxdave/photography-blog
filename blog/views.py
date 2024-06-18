a05781a499c4b25824322119c804ab2d480f544a
David Cotter <daveguy28@gmail.com>
2024-06-18 12:43:17 +0000

    index and base set up page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Photo
 
-# Create your views here.
+class PostList(generic.ListView):
+    model = Photo
+    queryset = Photo.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
