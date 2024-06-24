29ab838a753441e874492158fc461faaabc88a08
David Cotter <daveguy28@gmail.com>
2024-06-24 13:39:38 +0000

    templates blog problem with home page

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
 from .views import PhotoListView, PhotoDetailView, signup_view, homepage_view, about
 from . import views
 
 urlpatterns = [
-    path('photo/', views.PhotoListView.as_view(), name='photo_list'),
-    path('photo/<pk>/', views.PhotoDetailView.as_view(), name='photo_detail'),
+    path('', views.homepage_view, name='homepage'),
     path('signin/', views.SigninView.as_view(), name='signin'),
-    path('signout/', views.SignoutView.as_view(), name='signout'),
     path('signup/', views.signup_view, name='signup'),
-    path('homepage/', views.homepage_view, name='homepage'),
-    path('blog/', views.blog_view, name='blog'),
+    path('signout/', views.SignoutView.as_view(), name='signout'),
     path('about/', views.about, name='about'),
+    path('blog/', views.blog_view, name='blog'),
+    path('photo/<pk>/', views.PhotoDetailView.as_view(), name='photo_detail'),
 ]
--- photoblog/settings.py
+++ photoblog/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'photoblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
