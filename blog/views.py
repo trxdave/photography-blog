c1db2fa730761206e811fe092db98c4c9ce623e8
David Cotter <daveguy28@gmail.com>
2024-07-03 15:43:11 +0000

    move 404 to photoblog and add more files

--- blog/views.py
+++ blog/views.py
@@ -62,7 +62,7 @@ def category_view(request, category):
         photos = Photo.objects.filter(category=category_obj)
         return render(request, 'blog/category.html', {'photos': photos, 'category': category})
     else:
-        return render(request, 'blog/404.html')
+        return render(request, 'errors/404.html')
 
 def upload_landscape_image(request):
     if request.method == 'Post':
@@ -77,3 +77,23 @@ def upload_landscape_image(request):
 def landscape_image_list(request):
     images = LandscapeImage.objects.all()
     return render(request, 'landscape_image_list.html', {'images': images})
+
+
+def handler400(request, exception):
+    """ Error Handler 400 - Bad Request """
+    return render(request, "errors/400.html", status=400)
+
+
+def handler403(request, exception):
+    """ Error Handler 403 - Forbidden """
+    return render(request, "errors/403.html", status=403)
+
+
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)
+
+
+def handler500(request):
+    """ Error Handler 500 - Internal Server Error """
+    return render(request, "errors/500.html", status=500)
