116b1d62b5343ae433e45973dcfdb693f989c63b
David Cotter <daveguy28@gmail.com>
2024-08-13 13:45:03 +0000

    view for other username and own photo is allow to delete and edit

--- blog/views.py
+++ blog/views.py
@@ -231,13 +231,13 @@ def success(request):
 
 # Error Handlers
 def handler400(request, exception):
-    return render(request, 'errors/400.html')
+    return render(request, 'errors/400.html', status=404)
 
 def handler403(request, exception):
-    return render(request, 'errors/403.html')
+    return render(request, 'errors/403.html', status=403)
 
 def handler404(request, exception):
-    return render(request, 'errors/404.html')
+    return render(request, 'errors/404.html', status=404)
 
 def handler500(request):
-    return render(request, 'errors/500.html')
+    return render(request, 'errors/500.html', status=500)
