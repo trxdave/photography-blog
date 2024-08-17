4c2d050d31f84b86f8dfc0466b5e7137e4e8b5ef
David Cotter <daveguy28@gmail.com>
2024-08-17 10:03:55 +0000

    env.py change cloudinary

--- photoblog/settings.py
+++ photoblog/settings.py
@@ -182,3 +182,9 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'your_cloud_name',
+    'API_KEY': 'your_api_key',
+    'API_SECRET': 'your_api_secret'
+}
